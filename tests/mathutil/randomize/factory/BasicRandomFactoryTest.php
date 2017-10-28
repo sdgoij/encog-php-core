a9dbce90bb06b52006c58866d32506f72308c88a
Tim Jurcka <timjurcka@gmail.com>
2017-10-28 18:33:26 +0200

    Test BasicRandomFactory class

---
+++ tests/mathutil/randomize/factory/BasicRandomFactoryTest.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+namespace encog\test\mathutil\randomize\factory;
+
+use encog\mathutil\randomize\factory\BasicRandomFactory;
+use encog\util\Random;
+use PHPUnit_Framework_TestCase as TestCase;
+
+class BasicRandomFactoryTest extends TestCase {
+	public function testFactorFactory() {
+		$this->assertEquals(
+			(new Random((new Random((new Random(1))->nextLong()))->nextLong()))->nextLong(),
+			(new BasicRandomFactory(1))->factorFactory()->factor()->nextLong()
+		);
+	}
+}
