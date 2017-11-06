86af849821544840f35872bf5b9bd99efb304490
Tim Jurcka <timjurcka@gmail.com>
2017-11-06 02:21:27 +0100

    Implement Encog binary file format (*.egb)

---
+++ src/ml/data/buffer/EncogEGBFile.php
@@ -0,0 +1,194 @@
+<?php
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+namespace encog\ml\data\buffer;
+
+use SplFileInfo;
+use SplFileObject;
+
+/**
+ * Used to access or create an Encog Binary Training file (*.EGB).
+ */
+class EncogEGBFile {
+
+	const DOUBLE_SIZE = 8;
+	const HEADER_SIZE = self::DOUBLE_SIZE * 3;
+
+	public function __construct(SplFileInfo $file) {
+		if (!$file instanceof SplFileObject) {
+			$file = $file->openFile("c+b");
+		}
+		$this->file = $file;
+	}
+
+	public function __destruct() {
+		if ($this->file) {
+			$this->file->fflush();
+			$this->file = null;
+		}
+	}
+
+	public function create(int $input, int $ideal) {
+		if (!$this->file->ftruncate(0)) {
+			throw new BufferedDataError("Unable to truncate file.");
+		}
+		$this->file->rewind();
+		assert(24 == $this->file->fwrite(pack("c8d2",
+			ord('E'),
+			ord('N'),
+			ord('C'),
+			ord('O'),
+			ord('G'),
+			ord('-'),
+			ord('0'),
+			ord('0'),
+			$input,
+			$ideal
+		)));
+		$this->recordValues = $input + $ideal + 1;
+		$this->recordSize = $this->recordValues * self::DOUBLE_SIZE;
+		$this->numRecords = 0;
+		$this->input = $input;
+		$this->ideal = $ideal;
+	}
+
+	public function open() {
+		$header = (false !== $data = @unpack("c8id/dinput/dideal", $this->file->fread(24)))
+			? array_values($data)
+			: array_fill(0, 8, 0)
+		;
+		$version = chr($header[6]) . chr($header[7]);
+		$isEncogFile =
+			chr($header[0]) == 'E' &&
+			chr($header[1]) == 'N' &&
+			chr($header[2]) == 'C' &&
+			chr($header[3]) == 'O' &&
+			chr($header[4]) == 'G' &&
+			chr($header[5]) == '-'
+		;
+		if (!$isEncogFile) {
+			throw new BufferedDataError("File is not a valid Encog binary file: {$this->file->getFilename()}");
+		}
+		if (!is_numeric($version)) {
+			throw new BufferedDataError("File has invalid version number.");
+		}
+		if (intval($version) > 0) {
+			throw new BufferedDataError("File is from a newer version of Encog than is currently in use.");
+		}
+
+		$this->input = (int)$header[8];
+		$this->ideal = (int)$header[9];
+		$this->recordValues = $this->input + $this->ideal + 1;
+		$this->recordSize = $this->recordValues * self::DOUBLE_SIZE;
+		$this->numRecords = $this->recordSize > 0
+			? (int)(($this->size()-self::HEADER_SIZE) / $this->recordSize) : 0;
+	}
+
+	public function getInputCount(): int {
+		return $this->input;
+	}
+
+	public function getIdealCount(): int {
+		return $this->ideal;
+	}
+
+	public function getNumberOfRecords(): int {
+		return $this->numRecords;
+	}
+
+	public function getNumberOfRecordValues(): int {
+		return $this->recordValues;
+	}
+
+	public function getRecordSize(): int {
+		return $this->recordSize;
+	}
+
+	public function read(): float {
+		return unpack("e", $this->file->fread(self::DOUBLE_SIZE))[1];
+	}
+
+	public function readArray(array &$data) {
+		$data = array_values(unpack("e*", $this->file->fread(count($data))));
+	}
+
+	public function readColumn(int $row, int $column): float {
+		return $this->seek($row, $column)->read();
+	}
+
+	public function readRow(int $row): array {
+		$data = array_fill(0, $this->recordSize-1, 0.0);
+		$this->seek($row, 0)->readArray($data);
+		return $data;
+	}
+
+	public function write(float $value) {
+		$this->file->fwrite(pack("e", $value));
+	}
+
+	public function writeArray(array $values) {
+		$this->file->fwrite(pack("e*", ...$values));
+	}
+
+	public function writeByte(int $value) {
+		$this->file->fwrite(pack("c", $value));
+	}
+
+	public function writeColumn(int $row, int $column, float $value) {
+		$this->seek($row, $column)->write($value);
+	}
+
+	public function writeRow(int $row, array $values) {
+		$this->seek($row, 0)->writeArray($values);
+	}
+
+	private function seek(int $row, int $column): self {
+		$offset = self::HEADER_SIZE + ($row * $this->recordSize) + ($column * self::DOUBLE_SIZE);
+		$size = $this->size();
+		if ($offset > $size) {
+			$n = $offset-$size;
+			$this->file->fseek(0, SEEK_END);
+			$this->file->fwrite(pack("x$n"));
+		} else {
+			$this->file->fseek($offset);
+		}
+		return $this;
+	}
+
+	private function size(): int {
+		$pos = $this->file->ftell();
+		$this->file->fseek(0, SEEK_END);
+		$size = $this->file->ftell();
+		$this->file->fseek($pos);
+		return $size;
+	}
+
+	/** @var SplFileObject */
+	private $file;
+
+	/** @var int */
+	private $input;
+
+	/** @var int */
+	private $ideal;
+
+	/** @var int */
+	private $recordValues;
+
+	/** @var int */
+	private $recordSize;
+
+	/** @var int */
+	private $numRecords;
+}
---
+++ tests/ml/data/buffer/EncogEGBFileTest.php
@@ -0,0 +1,243 @@
+<?php
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+namespace encog\test\ml\data\buffer;
+
+use encog\ml\data\buffer\BufferedDataError;
+use encog\ml\data\buffer\EncogEGBFile;
+use encog\test\util\csv\MemoryStream;
+use PHPUnit_Framework_MockObject_MockObject as MockObject;
+use PHPUnit\Framework\TestCase;
+use RuntimeException;
+use SplFileInfo;
+use SplFileObject;
+
+class EncogEGBFileTest extends TestCase {
+	public function testOpenFile() {
+		/** @var $file MockObject|SplFileInfo */
+		$file = $this->getMockBuilder(SplFileInfo::class)->disableOriginalConstructor()->getMock();
+		$file->expects($this->once())->method("openFile");
+		new EncogEGBFile($file);
+
+		$this->expectExceptionMessage("failed to open stream: No such file or directory");
+		$this->expectException(RuntimeException::class);
+		new EncogEGBFile(new SplFileInfo(sys_get_temp_dir()."/xxx/yyy/non-existing.egb"));
+	}
+
+	public function testCreateEGB() {
+		MemoryStream::register("egb");
+		$expect = pack("c8d2",
+			ord('E'),
+			ord('N'),
+			ord('C'),
+			ord('O'),
+			ord('G'),
+			ord('-'),
+			ord('0'),
+			ord('0'),
+			1, 1
+		);
+		$egb = new EncogEGBFile(new SplFileInfo("egb://create"));
+		$egb->create(1, 1);
+
+		$this->assertEquals($expect, file_get_contents("egb://create"));
+		$this->assertEquals(0, $egb->getNumberOfRecords());
+		$this->assertEquals(3, $egb->getNumberOfRecordValues());
+		$this->assertEquals(24, $egb->getRecordSize());
+		$this->assertEquals(1, $egb->getInputCount());
+		$this->assertEquals(1, $egb->getIdealCount());
+
+		$this->expectExceptionMessage("Unable to truncate file.");
+		$this->expectException(BufferedDataError::class);
+
+		/** @var $file MockObject|SplFileInfo */
+		$file = $this->getMockBuilder(SplFileObject::class)->setConstructorArgs(["php://output"])->getMock();
+		$file->expects($this->once())->method('ftruncate')->willReturn(false);
+		(new EncogEGBFile($file))->create(1, 1);
+	}
+
+	public function testOpenEGB() {
+		MemoryStream::put("open", base64_decode(
+			"RU5DT0ctMDAAAAAAAAAAQAAAAAAAAPA/AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAPA/AAAAAAAA8D8AAAAAAAAAAAAAAAAAAPA/AAAAAAAAAAAAAAAAAADwPwAAAAAAAAAAAAAAAAAA8D8AAAAAAADwPwAAAAAAAAAA"
+		), "egb");
+
+		$egb = new EncogEGBFile(new SplFileInfo("egb://open"));
+		$egb->open();
+
+		$this->assertEquals(2, $egb->getInputCount());
+		$this->assertEquals(1, $egb->getIdealCount());
+		$this->assertEquals(32, $egb->getRecordSize());
+		$this->assertEquals(4, $egb->getNumberOfRecordValues());
+		$this->assertEquals(3, $egb->getNumberOfRecords());
+	}
+
+	public function testOpenInvalidHeader() {
+		MemoryStream::put("open", "abc", "egb");
+		$this->expectException(BufferedDataError::class);
+		$this->expectExceptionMessageRegExp("/File is not a valid Encog binary file: .*/");
+		(new EncogEGBFile(new SplFileInfo("egb://open")))->open();
+	}
+
+	public function testOpenInvalidVersionNumber() {
+		MemoryStream::put("open", pack("c8d2",
+			ord('E'),
+			ord('N'),
+			ord('C'),
+			ord('O'),
+			ord('G'),
+			ord('-'),
+			ord('x'),
+			ord('y'),
+			1, 1
+		), "egb");
+
+		$this->expectException(BufferedDataError::class);
+		$this->expectExceptionMessage("File has invalid version number.");
+		(new EncogEGBFile(new SplFileInfo("egb://open")))->open();
+	}
+
+	public function testOpenNewerVersionNumber() {
+		MemoryStream::put("open", pack("c8d2",
+			ord('E'),
+			ord('N'),
+			ord('C'),
+			ord('O'),
+			ord('G'),
+			ord('-'),
+			ord('1'),
+			ord('0'),
+			1, 1
+		), "egb");
+
+		$this->expectException(BufferedDataError::class);
+		$this->expectExceptionMessage("File is from a newer version of Encog than is currently in use.");
+		(new EncogEGBFile(new SplFileInfo("egb://open")))->open();
+	}
+
+	public function testReadColumn() {
+		MemoryStream::put("open", base64_decode(
+			"RU5DT0ctMDAAAAAAAAAAQAAAAAAAAPA/AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAPA/AAAAAAAA8D8AAAAAAAAAAAAAAAAAAPA/AAAAAAAAAAAAAAAAAADwPwAAAAAAAAAAAAAAAAAA8D8AAAAAAADwPwAAAAAAAAAA"
+		), "egb");
+
+		$egb = new EncogEGBFile(new SplFileInfo("egb://open"));
+		$egb->open();
+
+		$this->assertEquals(0, $egb->readColumn(0, 0));
+		$this->assertEquals(0, $egb->readColumn(0, 1));
+		$this->assertEquals(0, $egb->readColumn(0, 2));
+
+		$this->assertEquals(0, $egb->readColumn(1, 0));
+		$this->assertEquals(1, $egb->readColumn(1, 1));
+		$this->assertEquals(1, $egb->readColumn(1, 2));
+
+		$this->assertEquals(1, $egb->readColumn(2, 0));
+		$this->assertEquals(0, $egb->readColumn(2, 1));
+		$this->assertEquals(1, $egb->readColumn(2, 2));
+
+		$this->assertEquals(1, $egb->readColumn(3, 0));
+		$this->assertEquals(1, $egb->readColumn(3, 1));
+		$this->assertEquals(0, $egb->readColumn(3, 2));
+	}
+
+	public function testReadRow() {
+		MemoryStream::put("open", base64_decode(
+			"RU5DT0ctMDAAAAAAAAAAQAAAAAAAAPA/AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAPA/AAAAAAAA8D8AAAAAAAAAAAAAAAAAAPA/AAAAAAAAAAAAAAAAAADwPwAAAAAAAAAAAAAAAAAA8D8AAAAAAADwPwAAAAAAAAAA"
+		), "egb");
+
+		$egb = new EncogEGBFile(new SplFileInfo("egb://open"));
+		$egb->open();
+
+		$this->assertEquals([0,0,0], $egb->readRow(0));
+		$this->assertEquals([0,1,1], $egb->readRow(1));
+		$this->assertEquals([1,0,1], $egb->readRow(2));
+		$this->assertEquals([1,1,0], $egb->readRow(3));
+	}
+
+
+	public function testWrite() {
+		MemoryStream::register("egb");
+
+		$file = new SplFileInfo("egb://write");
+		$egb = new EncogEGBFile($file);
+		$egb->create(2, 1);
+		$egb->write(1);
+		$egb->write(2);
+		$egb->write(3);
+
+		$this->assertEquals(
+			"RU5DT0ctMDAAAAAAAAAAQAAAAAAAAPA/AAAAAAAA8D8AAAAAAAAAQAAAAAAAAAhA",
+			base64_encode(file_get_contents("egb://write"))
+		);
+	}
+
+	public function testWriteByte() {
+		MemoryStream::register("egb");
+
+		$file = new SplFileInfo("egb://write-byte");
+		$egb = new EncogEGBFile($file);
+		$egb->create(2, 1);
+		$egb->writeByte(ord('a'));
+		$egb->writeByte(ord('n'));
+		$egb->writeByte(ord('u'));
+
+		$this->assertEquals(
+			"RU5DT0ctMDAAAAAAAAAAQAAAAAAAAPA/YW51",
+			base64_encode(file_get_contents("egb://write-byte"))
+		);
+	}
+
+	public function testWriteArray() {
+		MemoryStream::register("egb");
+
+		$file = new SplFileInfo("egb://write-array");
+		$egb = new EncogEGBFile($file);
+		$egb->create(2, 1);
+		$egb->writeArray([1,2,3]);
+		$this->assertEquals(
+			"RU5DT0ctMDAAAAAAAAAAQAAAAAAAAPA/AAAAAAAA8D8AAAAAAAAAQAAAAAAAAAhA",
+			base64_encode(file_get_contents("egb://write-array"))
+		);
+	}
+
+	public function testWriteColumn() {
+		MemoryStream::register("egb");
+
+		$file = new SplFileInfo("egb://write-column");
+		$egb = new EncogEGBFile($file);
+		$egb->create(2, 1);
+
+		$egb->writeColumn(2, 2, 3);
+		$egb->writeColumn(1, 1, 2);
+		$egb->writeColumn(0, 0, 1);
+
+		$this->assertEquals(
+			"RU5DT0ctMDAAAAAAAAAAQAAAAAAAAPA/AAAAAAAA8D8AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIQA==",
+			base64_encode(file_get_contents("egb://write-column"))
+		);
+	}
+
+	public function testWriteRow() {
+		MemoryStream::register("egb");
+
+		$file = new SplFileInfo("egb://write-row");
+		$egb = new EncogEGBFile($file);
+		$egb->create(2, 1);
+		$egb->writeRow(3, [1,2,3]);
+		$egb->writeRow(1, [3,2,1]);
+		$this->assertEquals(
+			"RU5DT0ctMDAAAAAAAAAAQAAAAAAAAPA/AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIQAAAAAAAAABAAAAAAAAA8D8AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA8D8AAAAAAAAAQAAAAAAAAAhA",
+			base64_encode(file_get_contents("egb://write-row"))
+		);
+	}
+}
