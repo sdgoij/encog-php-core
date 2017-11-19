7b835d95317700e8c14af193168ec6b40ac46cf1
Tim Jurcka <timjurcka@gmail.com>
2017-11-19 23:19:02 +0100

    Completely test NetworkCODEC

--- tests/neural/networks/structure/NetworkCODECTest.php
+++ tests/neural/networks/structure/NetworkCODECTest.php
@@ -14,6 +14,7 @@
  */
 namespace encog\test\neural\networks\structure;
 
+use encog\mathutil\randomize\RangeRandomizer;
 use encog\ml\MLEncodable;
 use encog\ml\MLMethod;
 use encog\neural\networks\structure\NetworkCODEC;
@@ -25,20 +26,42 @@ class NetworkCODECTest extends TestCase {
 	use PrivateConstructorTest;
 
 	public function testNetworkSize() {
-		$this->assertEquals(5, NetworkCODEC::networkSize(
-			new class([1,2,3,4,5]) implements MLMethod, MLEncodable {
-				public function __construct(array $weights = []) { $this->weights = $weights; }
-				public function encodedArrayLength(): int { return count($this->weights); }
-				public function encodeToArray(array &$data) { $data = $this->weights; }
-				public function decodeFromArray(array $data) { $this->weights = $data; }
-				private $weights;
-			}));
+		$this->assertEquals(5, NetworkCODEC::networkSize($this->createEncodableMethod()));
 		$this->expectException(NeuralNetworkError::class);
 		$this->expectExceptionMessageRegExp("/^This machine learning method cannot be encoded/");
 		NetworkCODEC::networkSize(new class implements MLMethod {});
 	}
 
+	public function testNetworkToArray() {
+		$this->assertEquals([1,2,3,4,5], NetworkCODEC::networkToArray($this->createEncodableMethod()));
+		$this->expectException(NeuralNetworkError::class);
+		$this->expectExceptionMessageRegExp("/^This machine learning method cannot be encoded/");
+		NetworkCODEC::networkToArray(new class implements MLMethod {});
+	}
+
+	public function testArrayToNetwork() {
+		$values = array_fill(0, 255, 0.0);
+		(new RangeRandomizer(-1.0, 1.0))->randomizeArray($values);
+		$method = $this->createEncodableMethod();
+		NetworkCODEC::arrayToNetwork($values, $method);
+
+		$this->assertEquals($values, NetworkCODEC::networkToArray($method));
+		$this->expectException(NeuralNetworkError::class);
+		$this->expectExceptionMessageRegExp("/^This machine learning method cannot be encoded/");
+		NetworkCODEC::arrayToNetwork([], new class implements MLMethod {});
+	}
+
 	protected function getSubjectClassName(): string {
 		return NetworkCODEC::class;
+	}
+
+	private function createEncodableMethod(): MLEncodable {
+		return new class([1,2,3,4,5]) implements MLMethod, MLEncodable {
+			public function __construct(array $weights = []) { $this->weights = $weights; }
+			public function encodedArrayLength(): int { return count($this->weights); }
+			public function encodeToArray(array &$data) { $data = $this->weights; }
+			public function decodeFromArray(array $data) { $this->weights = $data; }
+			private $weights;
+		};
 	}
 }
